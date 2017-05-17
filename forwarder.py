f84244243d409c32f4dea418c19352e507080341
Hiroshi ABE <hirolovesbeer@gmail.com>
2017-05-17 13:32:52 +0900

    change filter rules

--- forwarder.py
+++ forwarder.py
@@ -11,11 +11,10 @@ NR_LISTENERS = os.cpu_count()
 SO_REUSEPORT = 15
 
 BUFSIZE = 1024
-DST_HOST = "192.168.11.13"
 
 CONFIG = 'config.yml'
 
-def listener_work(num, dst_hosts, dst_filter_hosts, filter_list, port):
+def listener_work(num, config, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.setsockopt(socket.SOL_SOCKET, SO_REUSEPORT, 1)   # set SO_REUSEPORT
     s.bind(("", port))
@@ -24,15 +23,21 @@ def listener_work(num, dst_hosts, dst_filter_hosts, filter_list, port):
         while True:
             data, addr = s.recvfrom(BUFSIZE)
 
-            for dst in dst_filter_hosts:
-                # match filter list
-                if addr[0] in filter_list:
-                    continue
-                else:
+            # security only
+            if addr[0] in config['syslog']['src-security']:
+                # src securityからのsyslog
+                for dst in config['syslog']['dst-security-only']:
+                    # security-onlyへ飛ばす
+                    s.sendto(data, (dst, port))
+            else:
+                # mgmt(security以外)からのsyslog
+                for dst in config['syslog']['dst-mgmt-only']:
+                    # mgmt-onlyへ飛ばす
                     s.sendto(data, (dst, port))
 
-            for dst in dst_hosts:
-               s.sendto(data, (dst, port))
+            # all
+            for dst in config['syslog']['dst-all']:
+                s.sendto(data, (dst, port))
 
     except KeyboardInterrupt:
         print ("Crtl+C Pressed. Shutting down.")
@@ -41,13 +46,9 @@ def server():
     with open(CONFIG) as f:
         config = yaml.load(f)
 
-    dst_hosts = config['syslog-all']
-    dst_filter_hosts = config['syslog-filter']
-    filter_list = config['filter-list']
-
     processes = []
     for i in range(NR_LISTENERS):
-        p = Process(target=listener_work, args=(i, dst_hosts, dst_filter_hosts, filter_list, SYSLOG_PORT))
+        p = Process(target=listener_work, args=(i, config, SYSLOG_PORT))
         p.start()
         os.system("taskset -p -c %d %d" % ((i % os.cpu_count()), p.pid))
         processes.append(p)
